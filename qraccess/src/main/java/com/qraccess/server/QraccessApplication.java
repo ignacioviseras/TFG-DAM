e3f3bd3f0e3c36953be22ebb912341e52c04d432
antoprog <anto.programacion@gmail.com>
2023-04-17 19:15:31 +0200

    subida de proyecto server qraccess limpio

---
+++ qraccess/src/main/java/com/qraccess/server/QraccessApplication.java
@@ -0,0 +1,13 @@
+package com.qraccess.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QraccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QraccessApplication.class, args);
+	}
+
+}
