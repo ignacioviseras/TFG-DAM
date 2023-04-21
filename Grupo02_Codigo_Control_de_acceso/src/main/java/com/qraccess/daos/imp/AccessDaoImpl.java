f09338439cba1eaf26440d90eb295cb572812503
ignacioviseras <ignacio.viseras@gmail.com>
2023-04-21 17:17:33 +0200

    getCustomerByAccessesId
    creo q están bien los métodos

--- Grupo02_Codigo_Control_de_acceso/src/main/java/com/qraccess/daos/imp/AccessDaoImpl.java
+++ Grupo02_Codigo_Control_de_acceso/src/main/java/com/qraccess/daos/imp/AccessDaoImpl.java
@@ -3,6 +3,7 @@ package com.qraccess.daos.imp;
 import com.qraccess.daos.interfaces.AccessDao;
 import com.qraccess.entities.Access;
 import com.qraccess.entities.Admin;
+import com.qraccess.entities.Customer;
 
 public class AccessDaoImpl extends CrudDaoImpl<Access> implements AccessDao {
 
@@ -26,9 +27,18 @@ public class AccessDaoImpl extends CrudDaoImpl<Access> implements AccessDao {
 		return Access.class;
 	}
 
+	//creo q se hace asi
 	public Access getById(int id) {
-		return null;
+		return (Access)super.getById(id);
 	}
+	
+	//creo q se hace asi
+	public Customer getCustomerByAccessesId(int id) {
+		CustomerDaoImpl cs = new CustomerDaoImpl();
+		return (Customer)cs.getById(id);
+		
+	}
+	
 	
 	public Access updateEntity(Access entity, Access updates) {
 		entity.setAvailables(updates.getAvailables());
--- Grupo02_Codigo_Control_de_acceso/src/main/java/com/qraccess/daos/interfaces/AccessDao.java
+++ Grupo02_Codigo_Control_de_acceso/src/main/java/com/qraccess/daos/interfaces/AccessDao.java
@@ -2,9 +2,11 @@ package com.qraccess.daos.interfaces;
 
 import com.qraccess.entities.Access;
 import com.qraccess.entities.Admin;
+import com.qraccess.entities.Customer;
 
 public interface AccessDao extends CrudDao<Access>{
 
 	public void validate(int id, String hash);	
+	public Customer getCustomerByAccessesId(int id);
 	
 }
