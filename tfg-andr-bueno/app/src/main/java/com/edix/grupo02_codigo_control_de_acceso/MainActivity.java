4adebc22a286a7349d00a0ff26ac2a0f838b11c9
ignacioviseras <ignacio.viseras@gmail.com>
2023-05-06 11:13:25 +0200

    tfg en android

---
+++ tfg-andr-bueno/app/src/main/java/com/edix/grupo02_codigo_control_de_acceso/MainActivity.java
@@ -0,0 +1,14 @@
+package com.edix.grupo02_codigo_control_de_acceso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
