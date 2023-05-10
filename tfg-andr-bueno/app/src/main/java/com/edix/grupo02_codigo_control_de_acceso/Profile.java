436ad0fdcd43dcdfeb5b4dfe9fcf4fffca187ac7
ignacioviseras <ignacio.viseras@gmail.com>
2023-05-10 19:05:25 +0200

    funciona la comunicacion de todas las ventanas y crear cuentas

--- tfg-andr-bueno/app/src/main/java/com/edix/grupo02_codigo_control_de_acceso/Profile.java
+++ tfg-andr-bueno/app/src/main/java/com/edix/grupo02_codigo_control_de_acceso/Profile.java
@@ -1,19 +1,28 @@
 package com.edix.grupo02_codigo_control_de_acceso;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Profile extends AppCompatActivity {
+    private EditText TextEmail;
+    private String correo = "";
+    TextView cambiarContraseña, textCorreo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().hide();
+
+        textCorreo = findViewById(R.id.cambiarContraseña);
 
 
         ImageButton botonHome = findViewById(R.id.botonHome);
@@ -30,6 +39,7 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Profile.this, Search.class);
+                startActivity(intent);
             }
         });
 
@@ -38,7 +48,23 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Profile.this, Profile.class);
+                startActivity(intent);
             }
         });
+
+        cambiarContraseña = findViewById(R.id.cambiarContraseña);
+        cambiarContraseña.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                correo = textCorreo.getText().toString();
+                if(!correo.isEmpty()){
+                    Toast.makeText(Profile.this, "Error al recuperar el correo", Toast.LENGTH_LONG).show();
+                }else{
+
+                }
+
+            }
+        });
+
     }
 }
