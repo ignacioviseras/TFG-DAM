344fc8255d398b782f115b78f50b8da9bd64c701
antoprog <anto.programacion@gmail.com>
2023-05-20 17:41:46 +0200

    error en el cambio de ventana
    Se me habia olvidado cambiar una ventana que he estado haciendo pruebas con el botón search

--- tfg-andr-bueno/app/src/main/java/com/edix/grupo02_codigo_control_de_acceso/MainActivity.java
+++ tfg-andr-bueno/app/src/main/java/com/edix/grupo02_codigo_control_de_acceso/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         botonHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Search.class);
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
